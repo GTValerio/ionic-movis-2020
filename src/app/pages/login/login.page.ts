ba129026e5077ec056b7d5d2904b7013efaa1076
GTValerio <val.gali@hotmail.com>
2020-01-10 08:36:24 -0600

    Agregar lista

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,40 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthenticationService} from '../../services/authentication.service';
-import {ToastController} from '@ionic/angular';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.page.html',
-    styleUrls: ['./login.page.scss'],
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-    email: string;
-    password: string;
-    isSubmitting = false;
 
-    constructor(private authenticationService: AuthenticationService,
-                private router: Router,
-                private toastController: ToastController) {
-    }
+  constructor() { }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+    this.authenticationService.logIn  }
 
-    onLogIn() {
-        this.isSubmitting = true;
-        this.authenticationService.logIn(this.email, this.password).then(didLogin => {
-            this.isSubmitting = false;
-            if (didLogin) {
-                this.router.navigate(['auth']);
-            } else {
-                this.toastController.create({
-                    message: 'Login NO',
-                    duration: 2000
-                }).then(toast => {
-                    toast.present();
-                });
-            }
-        });
-    }
 }
